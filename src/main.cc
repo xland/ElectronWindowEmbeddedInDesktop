b615d027db72b87f3147493a510527b3158bc219
unknown <412588801@qq.com>
2024-04-29 16:58:25 +0800

    embed

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,9 @@
 #include <napi.h>
-Napi::String Hello(const Napi::CallbackInfo &info)
-{
-    Napi::Env env = info.Env();
-    return Napi::String::New(env, "world");
-}
+#include "embed.hpp"
 Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
-    exports.Set(Napi::String::New(env, "hello"), Napi::Function::New(env, Hello));
+    exports.Set(Napi::String::New(env, "embed"), Napi::Function::New(env, embed));
+    exports.Set(Napi::String::New(env, "unEmbed"), Napi::Function::New(env, unEmbed));
     return exports;
 }
 NODE_API_MODULE(NODE_GYP_MODULE_NAME, Init)
